790640d888d6f91cb0bfe1f986920074dcfbd7b7
Alfonsus Ardani <alfonsusac@gmail.com>
2025-01-06 22:39:30 +0700

    update web

--- test/index.js
+++ test/index.js
@@ -1,6 +1,1 @@
 import { AdventEvent } from "advent-event";
-
-await Bun.build({
-  entrypoints: ['./index.tsx'],
-  outdir: './build',
-});
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -38,7 +38,7 @@ function LucideSun(props: SVGProps<SVGSVGElement>) {
 }
 
 
-export function LucideMoon(props: SVGProps<SVGSVGElement>) {
+function LucideMoon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3a6 6 0 0 0 9 9a9 9 0 1 1-9-9"></path></svg>
   )
