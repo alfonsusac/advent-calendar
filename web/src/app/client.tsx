470ab0fd45bcda08cca6301f04901fbe1e6dafe5
Alfonsus Ardani <alfonsusac@gmail.com>
2025-01-07 08:11:02 +0700

    web: added more link

--- web/src/app/client.tsx
+++ web/src/app/client.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { AdventEvent } from "advent-event"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState, type SVGProps } from "react"
 
 export function TestPage() {
 
@@ -213,8 +213,13 @@ export function TestPage() {
         <div className="p-3 px-4 mt-4 bg-slate-500/30 rounded-t-md flex flex-col items-start gap-2 border border-white/5" >
           npm i advent-event
         </div>
-        <a className="block p-2 px-4 border border-white/10 rounded-b-md hover:bg-white/5 font-sans text-sm font-semibold" href="https://www.npmjs.com/package/advent-event" target="_blank">
+        <a className="block p-2 px-4 border border-white/10 hover:bg-white/5 font-sans text-sm font-semibold" href="https://www.npmjs.com/package/advent-event" target="_blank">
+          <MdiNpmVariantOutline className="inline mr-2 h-4 w-4 align-[-0.2rem] opacity-60" />
           Visit NPM Site
+        </a>
+        <a className="block p-2 px-4 border border-white/10 rounded-b-md hover:bg-white/5 font-sans text-sm font-semibold" href="https://www.npmjs.com/package/advent-event" target="_blank">
+          <MdiGithub className="inline mr-2 h-4 w-4 align-[-0.2rem] opacity-60" />
+          Visit repository
         </a>
 
       </div>
@@ -371,3 +376,16 @@ function TimeTraveler(
 const nodeId = (id: string) => {
   return document.getElementById(id)
 } 
+
+
+function MdiNpmVariantOutline(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M3 3v18h18V3H3m3 3h12v12h-3V9h-3v9H6V6z" fill="currentColor"></path></svg>
+  )
+}
+
+function MdiGithub(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"></path></svg>
+  )
+}
